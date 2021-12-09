4e53b333bab23c5c4f07873eef14cc132c2bc3d2
SaNzEt539 <95311995+SaNzEt539@users.noreply.github.com>
2021-12-09 15:32:04 +0300

    Add files via upload

---
+++ Задание 2/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace consoleTask_6_2
+{
+    class Program
+    {
+        static double[] Arr(int N)
+        {
+            Random rand = new Random();
+            double[] arr = new double[N];
+            for (int i=0; i<N;i++)
+            {
+                arr[i] = rand.NextDouble();
+            }
+            return arr;
+        }
+        static string Res(double[] arr)
+        {
+            string str = String.Empty;
+            double max;
+            max = arr[0];
+            foreach (double a in arr)
+            {
+                if (Math.Abs(max) > Math.Abs(a))
+                {
+                    max = a;
+                }
+            }
+            return max.ToString();
+        }
+        static string Out(double[] arr)
+        {
+            string str = String.Empty;
+            foreach(double a in arr)
+            {
+                str += a + " ";
+            }
+            return str;
+        }
+        static void Main(string[] args)
+        {
+            try
+            {                
+                Console.Write("Введите число элементов массива: ");
+                int N = int.Parse(Console.ReadLine());
+                double[] arr = Arr(N);
+                Console.WriteLine("Массив: {0}", Out(arr));
+                Console.WriteLine("Максимальное число: {0}", Res(arr));
+            }
+            catch(System.FormatException)
+            {
+                Console.WriteLine("Введено что-то не то");
+            }
+        }
+    }
+}
---
+++ Задание 4/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections;
+namespace consoleTask_6_4
+{
+    class Massive
+    {
+        int[][] arr;
+        Random rand = new Random();
+        public Massive(int n)
+        {
+            arr = new int[n][];
+            for (int i=0; i<n; i++)
+            {
+                arr[i] = new int[rand.Next(1, 10)];
+            }
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j< arr[i].Length; j++)
+                {
+                    arr[i][j] = rand.Next(-100, 100);
+                }
+            }
+        }
+        public string Show()
+        {
+            string str = String.Empty;
+            foreach (int[] a in arr)
+            {
+                foreach (int j in a)
+                {
+                    str += j + " ";
+                }
+                str += "\n";
+            }
+            return str;
+        }
+        public ArrayList ShowInterval(int a, int b)
+        {
+            ArrayList aList = new ArrayList();
+            foreach (int[] i in arr)
+            {
+                foreach (int j in i)
+                {
+                    if (j < a || j > b) 
+                    {
+                        aList.Add(j);
+                    }
+                }                
+            }
+            return aList;
+        }
+    }
+    class Program
+    {
+        static string ToStr(ArrayList aL)
+        {
+            string str = String.Empty;
+            for (int i =0; i<aL.Count; i++)
+            {
+                str += aL[i] + " ";
+            }
+            return str;
+        }
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Введите размер массива: ");
+                int N = int.Parse(Console.ReadLine());
+                Console.Write("Введите интервал A: ");
+                int a = int.Parse(Console.ReadLine());
+                Console.Write("Введите интервал B: ");
+                int b = int.Parse(Console.ReadLine());
+                Massive mas = new Massive(N);
+                Console.WriteLine("Массив:\n{0}", mas.Show());
+                ArrayList arrayList = mas.ShowInterval(a, b);
+                
+                Console.WriteLine("Результат: {0}", ToStr(arrayList));
+            }
+            catch(System.FormatException)
+            {
+                Console.WriteLine("Введено что-то не то");
+            }
+        }
+    }
+}
